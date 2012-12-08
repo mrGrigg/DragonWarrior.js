8eedfc6108a0fa5571a2bdd317665f27ecec1255
Joe Grigg <joe.grigg@gmail.com>
2012-12-07 22:31:02 -0700

    removing tiles

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-require(['require', 'config'], function(require) {
+require(['require'], function(require) {
     require(['backbone'], function() {
         require(['views/DragonWarriorView', 'modules/Router'], function(DragonWarriorView, Router) {
             var dragonWarriorView = new DragonWarriorView();
