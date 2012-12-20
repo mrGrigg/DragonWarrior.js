9ff4a8708cd5144b5ce53f6557c01701033ae612
Joe Grigg <joe.grigg@gmail.com>
2012-12-20 14:22:24 -0700

    Terrible at git

--- public/js/editor.js
+++
@@ -1,6 +0,0 @@
-require(['require'], function(require) {
-    require(['views/EditorView'], function(EditorView) {
-        var editorView = new EditorView();
-        editorView.render();
-    });
-});
