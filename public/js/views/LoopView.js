673705c8d89b1610d991736bafd3398b3f30fe98
Joe Grigg <joe.grigg@gmail.com>
2012-12-20 14:45:25 -0700

    Putting all movement on the character

---
+++ public/js/views/LoopView.js
@@ -0,0 +1,214 @@
+define(function(require) {
+    var Player = require('views/PlayerView')
+        , view;
+
+    view = Backbone.View.extend({
+            className: 'gameView'
+                    
+            , events: {
+                
+            }
+
+            , initialize: function() {
+                var that = this;
+
+                _.bindAll(this, 'gameLoop');
+
+                $(document).on('keydown', function(event) {
+                    //that.onKeydown(event);
+                    that._pressed[event.keyCode] = true;
+                    // console.log('keydown:', that._pressed);
+                });
+
+                $(document).on('keyup', function(event) {
+                    //that.onKeyup(event);
+                    //delete that._pressed[event.keyCode];
+                    // console.log('keyup:', that._pressed);
+                });
+
+                this.frame = 1; //Seed the frame rate to loop from 1 - 30
+                //this.fps = 1000/64; //~30 fps
+                this.fps = 1000/32; //~30 fps
+                //this.fps = 1000/100; //~10 fps
+                this.frameRate = Math.round(this.fps);
+
+                this.player = new Player();
+                //this.player = player.toJSON();
+                //this.game = new GameLoop(this.player.toJSON(), this.loop);
+            }
+
+            , render: function() {
+                this.drawPlayer();
+
+                this.loop = setInterval(this.gameLoop, this.fps);
+
+                return this;
+            }
+
+            , drawPlayer: function() {
+                this.$el.append('<div class="player" />');
+            }
+
+            , gameLoop: function() {
+                this.frame = this.frame < this.frameRate ? this.frame + 1 : 1; //Loop the frame count
+
+                this.gameLogic(); //Parse logic
+                this.gameEvents(); //Check events
+                this.gameRender(); //Render images
+            }
+
+            , gameLogic: function() {
+                //Parse game logic
+                
+                //Is move available
+                //Check for environment effects (damage, slow, poison)
+                //Check for encounters
+            }
+
+            , gameEvents: function() {
+
+            }
+
+            , gameRender: function() {
+                this.step();
+                this.move();
+                //Scroll the world
+            }
+
+            //Events
+            , move: function() {
+                if (this._pressed[this._left]) {
+                    this.player.xMove(-32);
+                    this.player.xMovement = -32;
+                    this.player.movementDirection = 'left';
+                    // console.log('left');
+                }
+
+                if (this._pressed[this._up]) {
+                    this.player.yMove(-32);
+                    this.player.yMovement = -32;
+                    this.player.movementDirection = 'up';
+                    // console.log('up');
+                }
+
+                if (this._pressed[this._right]) {
+                    this.player.xMove(32);
+                    this.player.xMovement = 32;
+                    this.player.movementDirection = 'right';
+                    // console.log('right');
+                }
+
+                if (this._pressed[this._down]) {
+                    this.player.yMove(32);
+                    this.player.yMovement = 32;
+                    this.player.movementDirection = 'down';
+                    // console.log('down');
+                }
+
+                this._pressed = {};
+
+                if (this.player.xMovement === 0 && this.player.yMovement === 0) {
+                    return false; //no movement
+                }
+
+                if (! this.moveAvailable()) {
+                    this.player.xMovement = 0;
+                    this.player.yMovement = 0;
+
+                    return false;
+                }
+
+                //console.log('movement:', this.player.xMovement, this.player.yMovement);
+
+                //Calculate new movement
+                this.player.x = this.player.x + this.player.xMovement;
+                this.player.y = this.player.y + this.player.yMovement;
+                
+                //Reset x and y movement
+                this.player.xMovement = 0;
+                this.player.yMovement = 0;
+
+                //delete that._pressed[event.keyCode];
+                this.direction();
+                this.$('.player').animate({
+                    top: this.player.y
+                    , left: this.player.x
+                }, 0);
+            }
+
+            //Logic
+            , moveAvailable: function() {
+                //Check for obstructions
+                var moveAvailable = true
+                    , position = {};
+
+                this.max = {
+                    top: 0,
+                    left: 0,
+                    right: this.$el.width() - 32,
+                    bottom: this.$el.height() - 32
+                };
+
+                position.left = this.player.x + this.player.xMovement;
+                position.top = this.player.y + this.player.yMovement;
+
+                if (position.top < this.max.top) moveAvailable = false;
+                if (position.top > this.max.bottom) moveAvailable = false;
+                if (position.left < this.max.left) moveAvailable = false;
+                if (position.left > this.max.right) moveAvailable = false;
+                
+                // console.log('move available:', moveAvailable, 'position:', position, 'max:', this.max);
+                
+                return moveAvailable;
+            }
+
+            //Render Functions
+            , step: function() {
+                var stepRate = Math.round(this.frameRate / 2);
+                var step = this.frame <= stepRate ? 0 : 1;
+
+                if (this.player.step != step) {
+                    this.player.step = step;
+
+                    this.setPlayerSprite(this.sprite4x2[this.player.movementDirection][this.player.step]);
+                }
+            }
+
+            , direction: function() {
+                this.setPlayerSprite(this.sprite4x2[this.player.movementDirection][this.player.step]);
+            }
+
+            // , walk: function(direction) {
+            //     this.player.movementDirection = direction;
+
+            //     this.setPlayerSprite(this.sprite4x2[this.player.movementDirection][this.player.step]);
+            //     this.move(direction);
+            // }
+
+            , setPlayerSprite: function(playerSprite) {
+                this.$('.player').css({ 'background-position': playerSprite });
+            }
+
+            //Sprites that walk in four directions with a two frame walk
+            , sprite4x2: {
+                'left': {
+                    0:  '0 -33px'
+                    , 1: '-32px -33px'
+                }
+                , 'up': {
+                    0: '-64px -33px'
+                    , 1: '-96px -33px'
+                }
+                , 'right': {
+                    0: '-65px 0'
+                    , 1: '-97px 0'
+                }
+                , 'down': {
+                    0: '0 0'
+                    , 1: '-32px 0'
+                }
+            }
+        });
+
+    return view;
+});
--- public/js/views/PlayerView.js
+++ public/js/views/PlayerView.js
@@ -1,17 +1,149 @@
 define(function() {
+    var that;
+
     return Backbone.View.extend({
-        className: 'player',
+        id: 'player'
+        
+        , initialize: function() {
+            that = this;
+            _.bindAll(this);
 
-        initialize: function() {
-            _.bindAll(this, 'keyHandler', 'animate');
+            this.currentKey = undefined; //records the current keypressed
+            this.charStep = true; //Adjusted for the two sprite walk
+            this.charSpeed = 600; //how fast the char moves
+            this.stageWidth = 640;
+            this.stageHeight = 480;
             
-            var keyHandler = _.throttle(this.keyHandler, 250);
-            $(document).bind('keydown', keyHandler);
-        },
+            $(document).keydown(this.handleKeyDown);
+            $(document).keyup(this.handleKeyUp);
+        }
 
-        render: function() {
+        , render: function() {
 
             return this;
+        }
+
+        , handleKeyDown: function(event) {
+            var hasQueue = this.$el.queue('fx').length >= 1;
+
+            if (! this.currentKey && ! hasQueue) {
+                this.currentKey = event.keyCode;
+
+                switch(event.keyCode) {
+                    case 38: this.moveCharacter('up');    break;
+                    case 39: this.moveCharacter('right'); break;
+                    case 40: this.moveCharacter('down');  break;
+                    case 37: this.moveCharacter('left');  break;
+                }
+            }
+        }
+
+        , handleKeyUp: function(event) {
+            //don't stop the walk if the player is pushing other buttons
+            //only stop the walk if the key that started the walk is released
+            if (event.keyCode === this.currentKey) {
+                this.currentKey = false;
+            }
+        }
+
+        , moveCharacter: function(direction) {
+            //a player could switch key mid-animation
+            //record the key that was down when animation started
+            var that = this
+                , moveDirection = {}
+                , animationSpeed = this.charSpeed / 3
+                , stepClass = this.setStepClass(direction)
+                , continueMove = this.setContinueMove(direction, this.currentKey);
+
+            //Initial step is right
+            if (this.stepCharacter()) {
+                stepClass('left');
+                setTimeout(function() {
+                    that.stepCharacter();
+                    stepClass('right');
+                }, animationSpeed);
+                setTimeout(function() {
+                    that.stepCharacter();
+                    stepClass('left');
+                }, animationSpeed * 2);
+            }
+            else {
+                stepClass('right');
+                setTimeout(function() {
+                    that.stepCharacter();
+                    stepClass('left');
+                }, animationSpeed);
+                setTimeout(function() {
+                    that.stepCharacter();
+                    stepClass('right');
+                }, animationSpeed * 2);
+            }
+
+            if (! that.checkMove(direction)) {
+                return false;
+            }
+
+            //Move the character
+            switch (direction) {
+                case 'down':
+                    moveDirection = { top: '+=32' };
+                    break;
+
+                case 'up':
+                    moveDirection = { top: '-=32' };
+                    break;
+
+                case 'left':
+                    moveDirection = { left: '-=32' };
+                    break;
+
+                case 'right':
+                    moveDirection = { left: '+=32' };
+                    break;
+            }
+
+            this.$el.animate(moveDirection , that.charSpeed , 'linear', continueMove);
+        }
+
+        , checkMove: function(direction) {
+            var characterTop = this.$el.position().top
+                , characterLeft = this.$el.position().left;
+
+            switch(direction) {
+                case 'down':
+                    return characterTop + 32 < this.stageHeight;
+
+                case 'up':
+                    return characterTop - 32 >= 0;
+
+                case 'left':
+                    return characterLeft -32 >= 0;
+
+                case 'right':
+                    return characterLeft + 32 < this.stageWidth;
+            }
+        }
+
+        , stepCharacter: function() {
+            this.charStep = ! this.charStep;
+
+            return this.charStep;
+        }
+
+        , setContinueMove: function(direction, movementKey) {
+            return function() {
+                if (that.currentKey === movementKey) {
+                    that.moveCharacter(direction);
+                }
+            };
+        }
+
+        , setStepClass: function(direction) {
+            return function(className) {
+                var stepClass = direction + '-' + className;
+                that.$el.removeClass();
+                that.$el.addClass(stepClass);
+            };
         }
     });
 });
